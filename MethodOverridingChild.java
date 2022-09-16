072cc91502d637200d9146c7895a02fdcd9f4aaa
NishPatel1 <97016195+NishPatel1@users.noreply.github.com>
2022-09-17 00:21:33 +0530

    Delete MethodOverridingChild.java

--- MethodOverridingChild.java
+++
@@ -1,7 +0,0 @@
-public class MethodOverridingChild extends MethodOverridingParent {
-    @Override
-    public void sample() {
-        // TODO Auto-generated method stub
-        System.out.println("In MethodOverridingChild class");;
-    }
-}
