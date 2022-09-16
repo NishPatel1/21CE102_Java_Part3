8eafed960aad491615c3b395708e1235e46baf97
NishPatel1 <97016195+NishPatel1@users.noreply.github.com>
2022-09-17 00:18:07 +0530

    Add files via upload

---
+++ Practical_3/TestPart33Impl.java
@@ -0,0 +1,10 @@
+public class TestPart33Impl {
+    public static void main(String[] args) {
+        Part33Impl p = new Part33Impl();
+        System.out.println("a = " + p.a + "\nb = " + p.b + "\nc = " + p.c + "\nd = " + p.d);
+        p.add();
+        p.div();
+        p.mul();
+        p.sub();
+    }
+}
