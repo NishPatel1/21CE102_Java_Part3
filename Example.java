65c9678841fd423610fe0d86d8dfda965f70930a
NishPatel1 <97016195+NishPatel1@users.noreply.github.com>
2022-09-17 00:23:08 +0530

    Delete Example.java

--- Example.java
+++
@@ -1,9 +0,0 @@
-import P1.UserDefinedPack;
-
-public class Example{
-	public static void main(String[] args){
-		UserDefinedPack a = new UserDefinedPack();
-		a.getdata();
-		System.out.println("Using a.add():- " + a.add());
-	}
-}
